60a40d3025dae5c3ce3ada148305ce092ca6cc16
biddut4 <mdbiddut970@gmail.com>
2023-11-18 16:24:47 +0600

    file create

---
+++ schema-generator.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Schema Generator
+ *
+ * Plugin Name: Schema Generator
+ * Plugin URI:  https://wordpress.org/plugins/schema-generator/
+ * Description: Enables the WordPress schema generator plugin .
+ * Version:     1.0.0
+ * Author:      WordPress Contributors
+ * Author URI:  https://github.com/biddut4
+ * License:     GPLv2 or later
+ * License URI: http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * Text Domain: schema-generator
+ * Domain Path: /languages
+ * Requires at least: 4.9
+ * Requires PHP: 5.2.4
+ */
+
+
+
+
+?>
